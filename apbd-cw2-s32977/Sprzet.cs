6055d4b5e52c035bd982436dab8ca523256a5761
Wiktor Sajnóg <wiktorsajnog@gmail.com>
2026-03-19 11:12:19 +0100

    Abstract class Sprzet added with basic fields and constructor

---
+++ apbd-cw2-s32977/Sprzet.cs
@@ -0,0 +1,18 @@
+﻿namespace apbd_cw2_s32977;
+
+public abstract class Sprzet
+{
+    private int id {get; set;}
+    private bool available {get; set;}
+    private String name {get; set;}
+    
+    private double pricePerDay {get; set;}
+
+    Sprzet(int id, bool available, String name, double pricePerDay)
+    {
+        this.id = id;
+        this.available = available;
+        this.name = name;
+        this.pricePerDay = pricePerDay;
+    }
+}
