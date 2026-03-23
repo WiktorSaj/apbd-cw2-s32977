019d12bf17290119b3d8364e16ca9bc215b0211a
Wiktor Sajnóg <wiktorsajnog@gmail.com>
2026-03-23 16:28:23 +0100

    DataBase class added

---
+++ apbd-cw2-s32977/DataBase.cs
@@ -0,0 +1,40 @@
+﻿namespace apbd_cw2_s32977;
+
+public class DataBase
+{
+    private List<User> users = new List<User>();
+    private List<Sprzet> sprzet = new List<Sprzet>();
+    public List<Rental> rentals = new List<Rental>();
+    
+    public List<User> getUsers()
+    {
+        return users;
+    }
+
+    public List<Sprzet> getSprzet()
+    {
+        return sprzet;
+    }
+
+    public List<Rental> getRentals()
+    {
+        return rentals;
+    }
+    
+    public void addUser(User user)
+    {
+        users.Add(user);
+    }
+
+    public void addSprzet(Sprzet sprzetItem)
+    {
+        sprzet.Add(sprzetItem);
+    }
+
+    public void addRental(Rental rental)
+    {
+        rentals.Add(rental);
+    }
+
+}
+    
