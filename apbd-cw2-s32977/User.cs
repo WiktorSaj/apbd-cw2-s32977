8391a82ac51df1297c3e953652b2893675eee8aa
Wiktor Sajnóg <wiktorsajnog@gmail.com>
2026-03-23 15:19:55 +0100

    Abstract class User added with basic fields and constructor

---
+++ apbd-cw2-s32977/User.cs
@@ -0,0 +1,17 @@
+﻿namespace apbd_cw2_s32977;
+
+public abstract class User
+{
+    private int id { get; set; }
+    
+    private string name { get; set; }
+    
+    private string surname { get; set; }
+    
+    User(int id, string name, string surname)
+    {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+}
