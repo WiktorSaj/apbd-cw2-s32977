f6249374aebe933ff7462f9e39254a1cc886ec0e
Wiktor Sajnóg <wiktorsajnog@gmail.com>
2026-03-24 19:41:16 +0100

    RentEquipment method added in Service

--- apbd-cw2-s32977/Service.cs
+++ apbd-cw2-s32977/Service.cs
@@ -11,6 +11,60 @@ public class Service
         this.dataBase = dataBase;
     }
 
+    public void RentEquipment(User user, Sprzet sprzet, DateTime start, DateTime expectedEnd)
+    {
+
+        if (!sprzet.available)
+        {
+            Console.WriteLine("Sprzęt niedostępny");
+            return;
+        }
+
+        if (CountRentsForUser(user) >= Limit(user))
+        {
+            Console.WriteLine("Osiągnięto limit wypożyczeń");
+            return;
+        }
+        
+        
+        dataBase.addRental(new Rental(user, sprzet, start, expectedEnd));
+        sprzet.available = false;
+        
+        Console.WriteLine("Zapisano wypożyczenie");
+        
+    }
+    
+
+
+
+
+
+    public int Limit(User user)
+    {
+        if (user is Student) 
+        {
+            return 2;
+        }
+        else
+        {
+            return 5;
+        }
+    }
+
+    public int CountRentsForUser(User user)
+    {
+        int count = 0;
+
+        foreach (Rental rental in dataBase.rentals)
+        {
+            if (user == rental.user && rental.realEnd == null)
+                count++;
+        }
+        
+        return count;
+
+    }
+
     
 
 }
